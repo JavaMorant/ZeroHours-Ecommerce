9f0b6f551888bf87ee0058d885a7e2c684b5498d
jmacgowan <macgowan.joseph@gmail.com>
2024-09-02 22:39:52 +0100

    Hopeful fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <div className="App">
-      <video className="background-video" autoPlay loop muted>
+      <video className="background-video" autoplay muted loop playsinline>
         <source src={"./Assets/vids/TrailerLarge.mp4"}  />
         Your browser does not support the video tag.
       </video>
