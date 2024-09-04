714f309ec75065182897706f702738dae25ac7f5
Awande Dibidi <ad352@st-andrews.ac.uk>
2024-09-04 01:58:31 +0100

    Ohhh brother:

--- frontend/src/content_option.js
+++ frontend/src/content_option.js
@@ -64,7 +64,7 @@ const contactConfig = {
   YOUR_EMAIL: 'zerohourbrand@outlook.com',
   YOUR_FONE: '(555)123-4567',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+    'Please contact us with any concers, questions or queries :)',
   YOUR_SERVICE_ID: 'service_id',
   YOUR_TEMPLATE_ID: 'template_id',
   YOUR_USER_ID: 'user_id',
