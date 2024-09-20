3e6cc02b523cdd155c4911557dcbc1c0d695338f
jmacgowan <macgowan.joseph@gmail.com>
2024-09-20 18:49:50 +0100

    Fixed

--- backend/app.py
+++ backend/app.py
@@ -180,8 +180,8 @@ def create_checkout_session():
         checkout_session = stripe.checkout.Session.create(
             line_items=line_items,
             mode='payment',
-            success_url= DOMAIN_NAME + '/checkout?success',
-            cancel_url= DOMAIN_NAME + '/checkout?canceled',
+            success_url= DOMAIN_NAME + '/success',
+            cancel_url= DOMAIN_NAME + '/canceled',
             automatic_tax={'enabled': True},
             billing_address_collection='required',
             shipping_address_collection={'allowed_countries': ['GB']},
